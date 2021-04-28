10d895c61fc6f0bcb4127d5f3dd07b72002fc54e
Abdalla <abdullamazen98@gmail.com>
2021-04-28 22:30:34 +0200

    yarn

--- src/pages/Openpositions.js
+++ src/pages/Openpositions.js
@@ -24,14 +24,14 @@ function SignIn() {
           <div className="max-w-6xl mx-auto px-4 sm:px-6">
             <div className=" pt-32 pb-12 md:pt-40 md:pb-20">
               {/* Page header */}
-              
+
               <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
                 <h1 className="h1 font-special">Ledige stillinger</h1>
               </div>
 
               <Career />
+              <Cta />
               <Newsletter />
-              
             </div>
           </div>
         </section>
